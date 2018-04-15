84abb41fe3da618496c7cd79f2fa63f16f055f57
Caroline Chocholak <carolinechocholak@guest-wireless-upc-1600-10-120-001-237.usc.edu>
2018-04-15 14:02:54 -0700

    fixed sql database

--- createDatabase.sql
+++ createDatabase.sql
@@ -8,7 +8,6 @@ userName varchar(50) not null
 );
 CREATE TABLE meeting (
 meetingID int(11) primary key not null auto_increment,
-meetingTime varchar(50) not null,
 meetingTime varchar(50) not null
 );
 
@@ -16,8 +15,8 @@ CREATE TABLE meeting_users(
 meetingId int(11) not null,
 userID varchar(50) not null,
 FOREIGN KEY fk1(meetingID) REFERENCES meeting(meetingID),
-FOREIGN KEY fk1(userID) REFERENCES users(userID)
-)
+FOREIGN KEY fk2(userID) REFERENCES users(userID)
+);
 CREATE TABLE Location (
   locationID int(11) primary key not null auto_increment,
   locName varchar(255) not null
@@ -25,7 +24,7 @@ CREATE TABLE Location (
 CREATE TABLE LocationReviews (
 	LocationReviewsId int(11) primary key not null auto_increment, 
 	locationID int(11) not null,
-	userID int(11) not null,
+	userID VARCHAR(50) not null,
     	review varchar(180) not null,
 	FOREIGN KEY fk1(locationID) REFERENCES Location(locationID),
 	FOREIGN KEY fk2(userID) REFERENCES users(userID)
@@ -36,9 +35,12 @@ CREATE TABLE Team (
 );
 
 CREATE TABLE TeamMembers(
-	userID INT(11) NOT NULL,
+	userID VARCHAR(50) NOT NULL,
 	teamID INT(11) NOT NULL,
 	FOREIGN KEY fk1(userID) REFERENCES users(userID),
 	FOREIGN KEY fk2(teamID) REFERENCES Team(teamID)
 );
+
+INSERT INTO users (userID, userName) VALUES ('chochola', 'Caroline Chocholak');
+INSERT INTO users (userID, userName) VALUES ('nicolebe', 'Nicole Bergman');
 
