57f33099f79cac147830b5cf65c072df54b3294d
Callum <callumpe@usc.edu>
2018-04-15 12:32:57 -0700

    changed primary key for users to char(gmail)

--- createDatabase.sql
+++ createDatabase.sql
@@ -3,7 +3,7 @@ DROP SCHEMA IF EXISTS Final;
 CREATE SCHEMA Final;
 USE Final;
 CREATE TABLE users (
-userID int(11) primary key not null auto_increment,
+userID varchar(50) primary key not null,
 userName varchar(50) not null
 );
 CREATE TABLE meeting (
