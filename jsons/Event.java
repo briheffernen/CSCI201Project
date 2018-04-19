2e938af867263935460a11360555328dc598c9f8
Callum <callumpe@usc.edu>
2018-04-19 00:16:02 -0700

    accounted for timeless events in adduserevents

--- jsons/Event.java
+++ jsons/Event.java
@@ -2,6 +2,7 @@ package jsons;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Locale;
 
@@ -9,11 +10,13 @@ public class Event {
 	private Calendar start;
 	private Calendar end;
 	private String recurrence;
-	SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.ENGLISH);
+	SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.ENGLISH);
 
 	
 	public Event(String s, String e, String d)
 	{
+		s=cleanDate(s);
+		e = cleanDate(e);
 		start = Calendar.getInstance();
 		end = Calendar.getInstance();
 		try {
@@ -25,13 +28,29 @@ public class Event {
 
 		recurrence = d;
 	}
-	
+	public static String cleanDate(String d)
+	{
+		String ans="";
+		String [] arr = d.replaceAll("T", " ").split("-");
+		for(int i = 0; i < arr.length-1;i++)
+			{
+				ans+=arr[i];
+				if(i <2)
+					ans+="-";
+			}
+		return ans;
+	}
 	public Schedule getSchedule(Calendar s, Calendar e)
 	{
 		Schedule sch = new Schedule(s,e);
 		if(recurrence.equals(""))
 		{
 			sch.addInterval(new Interval(start,end));
+		}
+		else{
+			ArrayList<Interval> bases = new ArrayList<Interval>();
+			for(int i =0; i < bases.size();i++)
+				sch.addInterval(bases.get(i));
 		}
 		return sch;
 	}
